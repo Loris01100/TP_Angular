0f36bf859fb711b4ecddfea2dcf315695b084229
Loris01100 <lolobach01100@gmail.com>
2024-12-17 12:09:51 +0100

    Version 17/12/2024 2ème commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
 
-export const routes: Routes = [{ path: 'tasks', component: TaskListComponent }];
+export const routes: Routes = [
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks', component: TaskListComponent },
+];
